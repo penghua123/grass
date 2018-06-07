67c90ea0d8bf46fbdd7ce7d7d219e4141b83c02c
penghua <710159804@qq.com>
2018-06-07 08:51:17 +0800

    pg insert Successfully

--- pg-learning/main.go
+++ pg-learning/main.go
@@ -15,6 +15,13 @@ const (
     dbname   = "test"
 )
 
+type Teacher struct {
+    ID  int
+    Age int
+    Name string
+    Site string
+}
+
 func main() {
     psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",host, port, user, password, dbname)
     db, err := sql.Open("postgres", psqlInfo)
@@ -29,4 +36,13 @@ func main() {
     }
 
     fmt.Println("Successfully connected!")
+
+    sqlStatement := `INSERT INTO teacher (id, age, name, site)  VALUES ($1, $2, $3, $4)  RETURNING id`
+    id := 1
+    err = db.QueryRow(sqlStatement, 3, 46, "Tom", "Shanghai").Scan(&id)
+
+    //if err != nil {
+    //    panic(err)
+    //}
+    fmt.Println("New record ID is:", id)
 }
