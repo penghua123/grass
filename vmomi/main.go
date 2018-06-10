485cc8e14e89974f9c0ca00e50a26dc6ec485751
penghua <710159804@qq.com>
2018-06-10 19:40:18 +0800

    display more information about the vCenter

--- vmomi/main.go
+++ vmomi/main.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"text/tabwriter"
 
 	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/units"
 	"github.com/vmware/govmomi/view"
 	"github.com/vmware/govmomi/vim25/mo"
 	"github.com/vmware/govmomi/vim25/soap"
@@ -54,5 +57,32 @@ func main() {
 	for _, vm := range vms {
 		fmt.Printf("%s: %s\n", vm.Summary.Config.Name, vm.Summary.Config.GuestFullName)
 	}
+
+	var hss []mo.HostSystem
+	err = v.Retrieve(ctx, []string{"HostSystem"}, []string{"summary"}, &hss)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print summary per host (see also: govc/host/info.go)
+
+	tw := tabwriter.NewWriter(os.Stdout, 2, 0, 2, ' ', 0)
+	fmt.Fprintf(tw, "Name:\tUsed CPU:\tTotal CPU:\tFree CPU:\tUsed Memory:\tTotal Memory:\tFree Memory:\t\n")
+
+	for _, hs := range hss {
+		totalCPU := int64(hs.Summary.Hardware.CpuMhz) * int64(hs.Summary.Hardware.NumCpuCores)
+		freeCPU := int64(totalCPU) - int64(hs.Summary.QuickStats.OverallCpuUsage)
+		freeMemory := int64(hs.Summary.Hardware.MemorySize) - (int64(hs.Summary.QuickStats.OverallMemoryUsage) * 1024 * 1024)
+		fmt.Fprintf(tw, "%s\t", hs.Summary.Config.Name)
+		fmt.Fprintf(tw, "%d\t", hs.Summary.QuickStats.OverallCpuUsage)
+		fmt.Fprintf(tw, "%d\t", totalCPU)
+		fmt.Fprintf(tw, "%d\t", freeCPU)
+		fmt.Fprintf(tw, "%s\t", (units.ByteSize(hs.Summary.QuickStats.OverallMemoryUsage))*1024*1024)
+		fmt.Fprintf(tw, "%s\t", units.ByteSize(hs.Summary.Hardware.MemorySize))
+		fmt.Fprintf(tw, "%d\t", freeMemory)
+		fmt.Fprintf(tw, "\n")
+	}
+
+	_ = tw.Flush()
 
 }
