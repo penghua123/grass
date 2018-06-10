af722cda803e9851b29a8298fed8594435242e2b
penghua <710159804@qq.com>
2018-06-10 12:00:52 +0800

    Control couchbase through get and set commond

--- demo/main.go
+++ demo/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
     "fmt"
+    "encoding/json"
     "github.com/couchbase/go-couchbase"
 )
+
+type Event struct{
+    Type string `json:"type"`
+    Name string `json:"name"`
+    Likes int `json:"likes"`
+
+}
+
+func NewEvent(name string) *Event{
+    return &Event{"Event",name,0}
+}
+
+func NewEventJson(jsonbytes []byte)(event *Event){
+    err := json.Unmarshal(jsonbytes,&event)
+    if err != nil {
+        fmt.Println(err)
+    }
+    return
+}
+
+
 
 func main(){
     bucket,err :=couchbase.GetBucket("http://hua:penghua@10.15.158.52:8091/","default","hua")
@@ -12,4 +34,21 @@ func main(){
     }
     defer bucket.Close()
     fmt.Println("Connected to Couchbase Bucket '%s'\n",bucket.Name)
+
+    event := NewEvent("Couchbase collect")
+    err =bucket.Set("cc2014", 0, event)
+    
+    event = NewEvent("Couspher India")
+    err =bucket.Set("gc2015", 0, event)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println("Set Successifully")
+
+    var eventSearch Event
+    err =bucket.Get("cc2014",&eventSearch)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println("Get Successifully:",&eventSearch)
 }
