1a606ed5eb211e7212b63f59b4236d502f0dbbc5
penghua <710159804@qq.com>
2018-09-04 11:38:36 +0800

    A little simple test

---
+++ pool/simpleTest/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type structR6 struct {
+	B1 [10000]int
+}
+
+var r6Pool = sync.Pool{
+	New: func() interface{} {
+		return new(structR6)
+	},
+}
+
+func usePool() {
+	startTime := time.Now()
+	for i := 0; i < 10000; i++ {
+		sr6 := r6Pool.Get().(*structR6)
+		sr6.B1[0] = 0
+		r6Pool.Put(sr6)
+	}
+	fmt.Println("pool Used:", time.Since(startTime))
+}
+func standard() {
+	startTime := time.Now()
+	for i := 0; i < 10000; i++ {
+		var sr6 structR6
+		sr6.B1[0] = 0
+	}
+	fmt.Println("standard Used:", time.Since(startTime))
+}
+func main() {
+	standard()
+	usePool()
+}
