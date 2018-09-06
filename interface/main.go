206c2fba9697b75295666662bf263c858d1d9ad5
penghua <710159804@qq.com>
2018-09-06 22:06:16 +0800

    A little sample to understand interface

---
+++ interface/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Abser interface {
+	Abs() float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	a = f
+	fmt.Println(a.Abs())
+}
