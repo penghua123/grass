c65c4dbd3d424afa7b8105664ea249bf9eb151a2
penghua <710159804@qq.com>
2018-12-27 22:29:28 +0800

    HostHandler test

---
+++ mux/demo3/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	Run()
+}
+
+func Run() {
+	var dir string
+	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+	flag.Parse()
+	// 初始化Router
+	r := mux.NewRouter()
+	// 指定host
+	r.HandleFunc("/host", HostHandler).Host("localhost")
+	// 带变量的url路由
+	http.ListenAndServe(":3000", r)
+}
+
+func HostHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "the host is localhost")
+}
