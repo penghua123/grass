3aa5abaeef236c5776fcaf56a4011cee834add26
penghua <710159804@qq.com>
2019-01-09 21:49:20 +0800

    A webservice test

---
+++ webservice/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "github.com/callistaenterprise/goblog/accountservice/service"
+)
+
+var appName = "accountservice"
+
+func main() {
+    fmt.Printf("Starting %v\n", appName)
+    service.StartWebServer("6767")
+}
